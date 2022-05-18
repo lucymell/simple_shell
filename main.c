2b8deaa01d000bbdaffb704746a733fe2ed52ecb
lucymell <lucy.mutuota@gmail.com>
2022-05-18 13:58:53 +0300

    added new files

--- main.c
+++ main.c
@@ -1,23 +1,73 @@
-#include "headersh.h"
+#include "main.h"
 
 /**
-* main - call the shell by Arturo and Juani
-* @ac: argument count
-* @av: argument vector
-* @env: external variable environment parsed by lines
-* Return: nothing
-*/
-int main(int ac, char **av, char **env)
+ * free_data - frees data structure
+ *
+ * @datash: data structure
+ * Return: no return
+ */
+void free_data(data_shell *datash)
 {
-	int status = 0;
-	char **en = NULL;
+	unsigned int i;
 
-	if (ac == 1)
+	for (i = 0; datash->_environ[i]; i++)
 	{
-		en = envdup(env);
-		status = simple_sh(av, &en);
-		freeenv(en);
-		return (status);
+		free(datash->_environ[i]);
 	}
-	return (0);
+
+	free(datash->_environ);
+	free(datash->pid);
+}
+
+/**
+ * set_data - Initialize data structure
+ *
+ * @datash: data structure
+ * @av: argument vector
+ * Return: no return
+ */
+void set_data(data_shell *datash, char **av)
+{
+	unsigned int i;
+
+	datash->av = av;
+	datash->input = NULL;
+	datash->args = NULL;
+	datash->status = 0;
+	datash->counter = 1;
+
+	for (i = 0; environ[i]; i++)
+		;
+
+	datash->_environ = malloc(sizeof(char *) * (i + 1));
+
+	for (i = 0; environ[i]; i++)
+	{
+		datash->_environ[i] = _strdup(environ[i]);
+	}
+
+	datash->_environ[i] = NULL;
+	datash->pid = aux_itoa(getpid());
+}
+
+/**
+ * main - Entry point
+ *
+ * @ac: argument count
+ * @av: argument vector
+ *
+ * Return: 0 on success.
+ */
+int main(int ac, char **av)
+{
+	data_shell datash;
+	(void) ac;
+
+	signal(SIGINT, get_sigint);
+	set_data(&datash, av);
+	shell_loop(&datash);
+	free_data(&datash);
+	if (datash.status < 0)
+		return (255);
+	return (datash.status);
 }
